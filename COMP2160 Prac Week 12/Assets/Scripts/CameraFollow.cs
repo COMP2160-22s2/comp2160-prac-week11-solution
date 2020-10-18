c08e0785c92248bfac402e457457a9943c0d580f
Malcolm Ryan <malcolm.ryan@gmail.com>
2020-10-18 15:05:08 +1100

    Implemented follow camera

---
+++ COMP2160 Prac Week 12/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Rigidbody targetRigidbody;
+
+    public float followDistance;
+
+    public float smoothing = 0.5f;
+    private Vector3 smoothDir = Vector3.zero;
+
+    void Start()
+    {
+        targetRigidbody = target.GetComponent<Rigidbody>();
+
+        Vector3 position = transform.position;
+        position.x = target.position.x;        
+        position.z = target.position.z;
+        transform.position = position;
+    }
+
+    void Update()
+    {
+        Vector3 position = transform.position;
+        position.x = target.position.x;        
+        position.z = target.position.z;
+
+        Vector3 dir = targetRigidbody.velocity;
+        dir.y = 0;
+        smoothDir = Vector3.Lerp(smoothDir, -dir * followDistance, smoothing);
+
+        position += smoothDir;
+
+        transform.position = position;
+
+        
+    }
+}
